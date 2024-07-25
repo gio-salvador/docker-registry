7a8912ef67ab89a78b40476da10171ad80de03fe
Giovanni Salvador <giovanni@salvador.cloud>
2024-07-25 23:18:00 +0100

    improvements

--- config/EXEMPLE_src_envvar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Environment variables for the Docker registry and Certbot setup
-EMAIL='your-email@your-domain.com'
-DOMAIN='your-domain.com'
-CF_API_TOKEN='your-cloudflare-api-token'
